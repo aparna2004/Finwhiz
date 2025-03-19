1ba9dce0e6d5ecf49b88f1ae3531a64670347f6d
Aparna Sathyamurthi Thambu <aparna.thambu@gmail.com>
2025-03-19 20:27:44 +0530

    Complete fun AddExpense()

--- app/src/main/java/com/example/expensetracker/AddExpense.kt
+++ app/src/main/java/com/example/expensetracker/AddExpense.kt
@@ -1,2 +1,95 @@
 package com.example.expensetracker
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import java.util.zip.DataFormatException
+
+
+@Composable
+fun AddExpense() {
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+            val (nameRow, list, card, topBar, add) = createRefs()
+            Image(painter = painterResource(id = R.drawable.ic_topbar), contentDescription = null,
+                modifier = Modifier.constrainAs(topBar) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                })
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 60.dp, start = 16.dp, end = 16.dp)
+                .constrainAs(nameRow) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                })
+            {
+                Text(
+                    text = "Add Expense",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.White,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.Center)
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.dots_menu), contentDescription = null,
+                    modifier = Modifier.align(Alignment.CenterEnd)
+                )
+            }
+            DataForm(modifier = Modifier.padding(16.dp).constrainAs(card) {
+                top.linkTo(nameRow.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            })
+        }
+    }
+}
+
+@Composable
+fun DataForm(modifier: Modifier) {
+    Column (
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .shadow(16.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(Color.White)
+            .padding(16.dp)
+            .height(100.dp)
+    ) {
+
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun AddExpensePreview() {
+    AddExpense()
+}
+
+
